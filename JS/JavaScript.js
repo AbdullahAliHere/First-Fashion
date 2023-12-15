0260727e2e63aedc0280475ddbb579d3f66a08fc
AbdullahAliHere <abdullah.ali.cfr@gmail.com>
2023-12-15 14:48:21 +0500

    txt

---
+++ JS/JavaScript.js
@@ -0,0 +1,15 @@
+var loader = document.querySelector(".loading")
+
+window.addEventListener("load", vanish);
+
+function vanish() {
+  loader.classList.add("disppear");
+}
+
+
+
+
+// function deletePromotion(button) {
+//     const promotion = button.parentElement;
+//     promotion.style.display = "none";
+// }
